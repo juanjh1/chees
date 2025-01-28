89c9e36917ace16dd322fea7f82bc167d3e1abb4
Pedro Purihuaman <pedropuriihuaman@gmail.com>
2025-01-28 13:18:59 -0500

    feat: add piece figures

--- frontend/src/components/Board.vue
+++ frontend/src/components/Board.vue
@@ -1,12 +1,62 @@
 <script setup lang="ts">
-import Cell from "./Cell.vue";
+import Piece from "./Piece.vue";
+
+interface Piece {
+	type: string | null;
+	color: string | null;
+	row: number;
+	col: string;
+}
+
+const COLUMNS: string[] = ["a", "b", "c", "d", "e", "f", "g", "h"];
+
+const generateMajorPiecesRow = (color: string, row: number): Piece[] => [
+	{ type: "rook", color, row, col: "a" },
+	{ type: "knight", color, row, col: "b" },
+	{ type: "bishop", color, row, col: "c" },
+	{ type: "queen", color, row, col: "d" },
+	{ type: "king", color, row, col: "e" },
+	{ type: "bishop", color, row, col: "f" },
+	{ type: "knight", color, row, col: "g" },
+	{ type: "rook", color, row, col: "h" },
+];
+
+const generatePawnRow = (color: string, row: number): Piece[] =>
+	COLUMNS.map((col: string) => ({ type: "pawn", color, row, col }));
+
+const generateEmptyRow = (row: number): Piece[] =>
+	COLUMNS.map((col: string) => ({ type: null, color: null, row, col }));
+
+const initialBoard: Piece[][] = [
+	generateMajorPiecesRow("black", 1),
+	generatePawnRow("black", 2),
+
+	generateEmptyRow(3),
+	generateEmptyRow(4),
+	generateEmptyRow(5),
+	generateEmptyRow(6),
+
+	generatePawnRow("white", 7),
+	generateMajorPiecesRow("white", 8),
+];
 </script>
 
 <template>
 	<div class="board">
-		<template v-for="i in 64" :key="i">
-			<Cell :content="i.toString()" />
-		</template>
+		<div
+			v-for="(row, rowIndex) in initialBoard"
+			:key="rowIndex"
+			class="board__row"
+		>
+			<template v-for="(col, _) in row" :key="`${col.row}-${col.col}`">
+				<Piece
+					:row="col?.row"
+					:col="col?.col"
+					:color="col?.color"
+					:type="col?.type"
+				/>
+			</template>
+		</div>
 	</div>
 </template>
 
@@ -15,7 +65,7 @@ import Cell from "./Cell.vue";
 	grid-area: center;
 
 	display: flex;
-	flex-wrap: wrap;
+	flex-direction: column;
 	width: var(--board-size);
 	height: var(--board-size);
 
@@ -26,5 +76,9 @@ import Cell from "./Cell.vue";
 		var(--color-odd) 270deg
 	);
 	background-size: calc(var(--cell-size) * 2) calc(var(--cell-size) * 2);
+}
+
+.board__row {
+	display: flex;
 }
 </style>
